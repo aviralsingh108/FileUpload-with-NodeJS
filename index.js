caea81f7544650f692bc41f6888953187d06007d
aviralsingh108 <admiralaviral@gmail.com>
2023-04-11 00:20:40 +0530

    users model added

--- index.js
+++ index.js
@@ -5,18 +5,25 @@ const path = require("path");
 
 //Connect DB using Mongoose
 mongoose
-  .connect("mongodb://localhost:27017/fileUpload", { useNewUrlParser: true })
+  .connect("mongodb://localhost:27017/fileUpload", {
+    useNewUrlParser: true,
+    useUnfiedToplogy: true,
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connectiong to MongoDB", err));
 
 //create express app
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, "static")));
-
 //Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use("/", express.static(path.join(__dirname, "static")));
+
+// API's
+app.post("/api/register", async (req, res) => {
+  console.log(req.body);
+});
 
 // start server
 const port = process.env.PORT || 3000;
