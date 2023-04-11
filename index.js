488fae87eff7e05c3be77c0fd6c838d8aa46dc40
aviralsingh108 <admiralaviral@gmail.com>
2023-04-11 15:17:07 +0530

    added user create user and hashing for password

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const path = require("path");
+const bcrypt = require("bcryptjs");
+
+const User = require("./models/user");
 
 //Connect DB using Mongoose
 mongoose
   .connect("mongodb://localhost:27017/fileUpload", {
     useNewUrlParser: true,
-    useUnfiedToplogy: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connectiong to MongoDB", err));
@@ -23,6 +26,17 @@ app.use("/", express.static(path.join(__dirname, "static")));
 // API's
 app.post("/api/register", async (req, res) => {
   console.log(req.body);
+  const { username, password: plainTextPassword } = req.body;
+  const password = await bcrypt.hash(plainTextPassword, 10);
+  try {
+    const response = await User.create({
+      username,
+      password,
+    });
+    console.log(response);
+  } catch (error) {
+    return res.json({ status: error });
+  }
 });
 
 // start server
